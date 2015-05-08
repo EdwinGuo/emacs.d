77b5fad829a157f41fa1cb767ba47a424c84259f
EdwinGuo <eguo@interset.com>
2015-05-08 17:30:55 -0400

    ensime scala hool

--- EdwinGuo/emacs.el
+++ EdwinGuo/emacs.el
@@ -1,3 +1,8 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
 (powerline-default-theme)
+
+(setq debug-on-server t)
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+(electric-pair-mode 1)
